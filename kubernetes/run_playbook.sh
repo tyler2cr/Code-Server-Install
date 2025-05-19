6d9f1972cb7d5cbaac9502a375c54f325d6f6738
tyler <tcr@enfuse.io>
2025-05-18 19:11:22 -0700

    l

--- kubernetes/run_playbook.sh
+++ kubernetes/run_playbook.sh
@@ -4,7 +4,9 @@ set -e
 
 # Run the playbook
 echo "Running Ansible playbook..."
-if ansible-playbook -i inventory.ini playbook.yml; then
+if ansible-playbook \
+    -i inventory.ini \
+    playbook.yml; then
     echo "Playbook completed successfully!"
 else
     echo "Playbook failed!"
